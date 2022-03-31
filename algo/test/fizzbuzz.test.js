af0b08c13944a498e66ee81aee1c03eda7be6d2a
Pierre Deltour <pierre.deltour@diamondbytes.com>
2022-03-31 19:27:11 +0200

    remove log

--- algo/test/fizzbuzz.test.js
+++ algo/test/fizzbuzz.test.js
@@ -4,7 +4,7 @@ let fizzbuzz = require("../src/fizzbuzz");
 describe("test fizzbuzz mutually exclusive function", () => {
     it("test fizzbuzz mutually exclusive function up to 15", () => {
         let res = fizzbuzz.mutuallyExclusiveFizzBuzz(15);
-        console.log(res);
+        //  console.log(res);
         expect(res).toEqual([1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz"]);
     });
 
--- algo/test/inc.test.js
+++ algo/test/inc.test.js
@@ -4,19 +4,19 @@ let inc = require("../src/inc");
 describe("test inc", () => {
     it("should return [1,2,4] for inc([1,2,3]", () => {
         let res = inc.inc([1, 2, 3]);
-        console.log(res);
+        //   console.log(res);
         expect(res).toEqual([1, 2, 4]);
     });
 
     it("should return [2,0] for inc([1,9]", () => {
         let res = inc.inc([1, 9]);
-        console.log(res);
+        //       console.log(res);
         expect(res).toEqual([2, 0]);
     });
 
     it("should return [1,0,0] for inc([9,9]", () => {
         let res = inc.inc([9, 9]);
-        console.log(res);
+        //       console.log(res);
         expect(res).toEqual([1, 0, 0]);
     });
 
